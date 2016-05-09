70e50272810d474ad946c0c524a72475f90836d1
kg <kjg310@gmail.com>
2016-05-09 14:21:57 -0400

    done?

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -5,10 +5,10 @@ var password = require('./password.js')
 // Setting up mySQL connection. 
 var connection = mysql.createConnection({
     port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: password,
-    database: 'burgers_db'
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'e4saaulijiioa75m',
+    password: 'p4vv075mw8ydqcb0',
+    database: 'k3qiifhqq0g3trnm'
 });
 
 // Connection with connection info. 
